150da9e8afefb698e76f868a16f97ea99666149f
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-07 05:39:40 -0500

    added html and css files

--- app.js
+++ app.js
@@ -48,6 +48,72 @@ function create() {
     // don't allow player to leave screen so add colition with world borders to true
     player.setCollideWorldBounds(true); 4
 
+    // build the running animation
+
+    // running left animation
+    this.anims.create({
+        key: "left",
+        // frames 0-3 for left
+        frames: this.anims.generateFrameNumbers("dude", {
+            start: 0,
+            end: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    // Front/turning sprite 
+    this.anims.create({
+        key: "turn",
+        frames: [{
+            key: "dude",
+            frame: 4
+        }],
+        frameRate: 20
+    });
+
+    // running right animation 
+    this.anims.create({
+        key: "right",
+        // frames 0-3 for right
+        frames: this.anims.generateFrameNumbers("dude", {
+            start: 5,
+            end: 8
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    // add a collision relation between player and platforms
+    this.physics.add.collider(player, platforms);
 
 }
+
+function update() {
+    keyboard = this.input.keyboard.createCursorKey();
+
+    if (keyboard.left.isDown) {
+        // if left arrow is pressed
+        player.setVelocityX(-160);
+        player.anims.play("left", true);
+
+    } else if (keyboard.right.isDown) {
+        // if right arrow is pressed
+        player.setVelocityX(160);
+        player.anims.play("right", true);
+
+    } else {
+        // if neither
+        player.setVelocityX(0);
+        player.anims.play("turn");
+
+    }
+
+    if (keyboard.up.isDown && player.body.touching.down) {
+        // if user jumps while on the ground
+        player.setVelocityY(-330);
+    }
+}
+
+
 
